f3c04ea84d30142bad560f8fb1d5393207f2aee3
arun <nemmadiarunkumar@gmail.com>
2025-09-24 11:20:51 +0530

    dream

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dream-Track/' // <-- your GitHub repo name here
+  server: {
+    // This is the magic line
+    historyApiFallback: true,
+  },
 });
