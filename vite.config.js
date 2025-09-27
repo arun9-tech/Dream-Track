83ddd8b39b3f3ea20a05b70634af1d09381526ab
arun <nemmadiarunkumar@gmail.com>
2025-09-27 09:44:00 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is the essential part that fixes the 404 error
-  server: {
-    historyApiFallback: true,
-  },
-});
+  base: '/Dream-Track/', // 👈 very important for GitHub Pages
+})
